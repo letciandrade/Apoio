8a2bf828545ee28b25b237e3ed6e99702b75e743
Letícia__Moraes <110117834+letciandrade@users.noreply.github.com>
2023-03-03 14:47:41 -0300

    Add files via upload

---
+++ fundamentos/1-1-1cuidadoNumber.js
@@ -0,0 +1,5 @@
+console.log(4 / 0)
+console.log("30" / 2)
+console.log("coisa!" * 4)
+console.log(0.1 + 0.7) //IEEE
+console.log(12.39487687.tofixed(2))
---
+++ fundamentos/1-variaveis.js
@@ -0,0 +1,9 @@
+var x = 21
+console.log(typeof x)
+
+var nome = "alice"
+console.log(typeof nome)
+
+var idade = prompt("digite sua idade.")
+console.log(idade)
+
