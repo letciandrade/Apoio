99a8bf24ab49c7f7b630026fd66be34c2fd80fde
Letícia__Moraes <110117834+letciandrade@users.noreply.github.com>
2023-05-29 18:09:38 -0300

    Add files via upload

---
+++ ProgramaçãoObjeto/Objeto.js
@@ -0,0 +1,45 @@
+//codigo nao executavel
+
+//precedual 
+processamento(valor1, valor2, valor3)
+
+//orientação ao objeto
+//objetos tem seu atributo e logo em seguida o valor 
+objeto = {
+    valor1:true,//valor primitivo
+    //quando o objeto tem multi valores.. se cria outro objeto dentro do objeto
+    //tiopo multivalorado
+    valor2:{
+        chave:true,
+    },
+    valor3:gnfg,
+    processamento(){
+
+    }
+}
+
+//padrao de estrutura de dados, isso é um vetor:
+//objeto = ['valor1','valor2','valor3']
+
+
+//principios da orientação ao objeto:
+
+// abstração: 
+//ex: para construir uma casa é necessario planejar ela com a planta da casa trazendo isso a abstração fica assim
+// é vc criar uma classe(planta) com todas aquelas carcteristicas para serem utilizadas no sistema. 
+
+// encapsulamento:
+//encapsalar funcionalidades, para a pessoa nao saber oque ta acontecendo.
+//exemplo: a gente nao ve o motor do carro funcionando nem suas funcionalidades, só ver oque ta acontecendo
+//para a pessoa nao entender oq ta acontecendo vendo o codigo.
+
+// herança:
+//tem uma classe e cria objetos apartir daquela caracteristica, mudando somente algumas coisas.
+// como uma casa de condominio que tem o mesmo padrao, so muda a cor ou alguma reforma noq quintal e tals
+
+
+//polimorfismo:
+//pega esse objeto e transforma esse obejto em varios outros objetos, 
+
+//toda propiedade de objeto tem chave e valor.
+//nos podemos colocar valores primitivos, multivalorados..
